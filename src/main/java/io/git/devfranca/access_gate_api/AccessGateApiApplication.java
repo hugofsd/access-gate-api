d4e13a667bd3c83d9e0e81198f505db2de75bb7a
hugofsd <hugo619@outlook.com>
2026-02-06 14:52:46 -0300

    init

---
+++ src/main/java/io/git/devfranca/access_gate_api/AccessGateApiApplication.java
@@ -0,0 +1,13 @@
+package io.git.devfranca.access_gate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessGateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessGateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/git/devfranca/access_gate_api/AccessGateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.git.devfranca.access_gate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessGateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
